973a68a16f4e9460a06dc2dce0043481e82b4eb6
Chadwick <chaddybox026@gmail.com>
2021-09-13 15:40:32 -0500

    v2.0 (now with css!)

--- contractUtils/contractProjection.js
+++ contractUtils/contractProjection.js
@@ -1,32 +1,43 @@
-var chkPop = 0, //chk
+var
+  chkPop = 0, //chk
   eggDel = 0, //egg
   chkRate = 0, //chk/min
+  numHabs = 1, //number of hatcheries
   rateCalm = 0, //level
   eggRate = 0, //egg/min
   eggPerChkRate = 0, //egg/chk/min
   due = 0; //hours
 
+let magnitudes = [
+  "M",
+  "B",
+  "T",
+  "q",
+  "Q",
+  "s",
+  "S",
+]
+
 //calculates projected egg delivery with given parameters
+//args: none
+//return: none
 function calcEggProj() {
 
   getData();
 
-  var dueMins = due*60;
+  var dueMins = due * 60; //convert hours to minutes
 
-  var calc = (getEggChkRate()*chkRate*getCalmBoost()/2)*(dueMins*dueMins);
-  console.log(calc);
-  calc = calc + getEggChkRate()*chkPop*dueMins;
-  console.log(calc);
-  calc = calc + eggDel*1;
-  console.log(calc);
+  var calc = ( getEggChkRate() * chkRate * getCalmBoost() * numHabs / 2 ) * ( dueMins * dueMins )
+    + ( getEggChkRate() * chkPop ) * dueMins
+    + eggDel * 1;
 
-  document.getElementById("output").innerHTML = Number.parseFloat(calc).toFixed(0);
-
+  document.getElementById("output").value = Number.parseFloat(calc).toExponential(2);
 
 }
 
 //calculates the boost of internal hatcheries due to calm upgrade
-//retuns a value between 1 and 3
+//args: none
+//retun: multiplier value between 1 and 3
 function getCalmBoost() {
 
   if (rateCalm > 20) return 3;
@@ -36,21 +47,36 @@ function getCalmBoost() {
 }
 
 //grabs data from HTML input fields
+//args: none
+//return: none
 function getData() {
 
-  chkPop = document.getElementById("_chkPop").value;
-  chkRate = document.getElementById("_chkRate").value;
-  rateCalm = document.getElementById("_rateCalm").value;
-  eggDel = document.getElementById("_eggDel").value;
-  eggRate = document.getElementById("_eggRate").value;
-  due = document.getElementById("_due").value;
+  chkPop = getFieldValue("_chkPop");
+  chkRate = getFieldValue("_chkRate");
+  rateCalm = getFieldValue("_rateCalm");
+  numHabs = getFieldValue("_numHabs");
+  if (numHabs < 1 || numHabs > 4) numHabs = 0; //avoids too many or too few habs
+  eggDel = getFieldValue("_eggDel");
+  eggRate = getFieldValue("_eggRate");
+  due = getFieldValue("_due");
 
 }
 
 //calculates the rate of eggs per chicken per minute
+//no arg
+//return: rate num
 function getEggChkRate() {
 
   if (chkPop <= 0) return 0;
   else return eggRate/chkPop; //egg/chk/min
+
+}
+
+//grabs the fields from arg id
+//args: id, string of HTML id
+//return: value of field in HTML
+function getFieldValue(id) {
+
+  return (document.getElementById(id).value) * 1; //*1 ensures number
 
 }
